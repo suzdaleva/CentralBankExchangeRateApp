391f15462216c48975c2402b635cb804dbf588c5
Koval Anastasiya <suzdaleva.nastasiya@gmail.com>
2022-02-19 12:15:03 +0300

    empty database error fixed

--- app/src/main/java/com/manicpixie/cfttest/domain/repository/CFTRepository.kt
+++ app/src/main/java/com/manicpixie/cfttest/domain/repository/CFTRepository.kt
@@ -9,4 +9,5 @@ import com.manicpixie.cfttest.data.remote.dto.CurrencyDto
 interface CFTRepository {
      suspend fun getCurrencies(): List<CurrencyDto>
      suspend fun getCurrencyByName(name: String): CurrencyEntity
+     suspend fun numberOfEntries(): Int
 }
